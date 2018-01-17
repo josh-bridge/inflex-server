b6a5b6877faeda3ace226283603f8f9342545ff5
josh-bridge <14032908@stu.mmu.ac.uk>
2018-01-17 12:19:45 +0000

    Initial app version - flask, mako

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_mako import MakoTemplates, render_template
 
 application = Flask(__name__)
+mako = MakoTemplates(application)
+
+STATIC = 'static'
 
 
 @application.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('helloworld.html', text='Welcome to api.inflex.co')
 
 
 if __name__ == '__main__':
